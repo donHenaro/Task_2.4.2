2f05d275a496258b4133fc75491400ca1bb8c640
Андрей Косачёв <mgb@tut.by>
2021-08-19 17:05:24 +0300

    Task_2.4.2 Исправлены замечания JM

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -26,8 +26,7 @@ public class User implements UserDetails {
    @Column(name = "password")
    private String password;
 
-   @ManyToMany(fetch = FetchType.EAGER)
-   @Fetch(FetchMode.JOIN)
+   @ManyToMany(fetch = FetchType.LAZY)
    @JoinTable(name = "users_roles",
            joinColumns = @JoinColumn(name = "users_id"),
            inverseJoinColumns = @JoinColumn(name = "roles_id"))
